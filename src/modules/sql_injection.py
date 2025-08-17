22b26e2e4b6091ee8c8ba305a505b6f992137e20
jpaullopes <jpaullopes27@gmail.com>
2025-08-17 10:40:31 -0300

    sql_injection module was improvemented

--- src/modules/sql_injection.py
+++ src/modules/sql_injection.py
@@ -1,6 +1,9 @@
 import requests
 
 def login_test(url, dictionary_login):
-    response = requests.post(url, data=dictionary_login)
-
-    print(response.text)
+    try:
+        response = requests.post(url, data=dictionary_login)
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return
