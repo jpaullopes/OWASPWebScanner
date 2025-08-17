c7bb6e44bb714c299440f494256979181b92d102
jpaullopes <jpaullopes27@gmail.com>
2025-08-17 11:41:48 -0300

    ht,l extractor and token extractor was be created

--- src/modules/sql_injection.py
+++ src/modules/sql_injection.py
@@ -1,4 +1,6 @@
 import requests
+from recon.spider import html_extractor
+from bs4 import BeautifulSoup
 
 def login_test(url, dictionary_login):
     try:
@@ -8,6 +10,13 @@ def login_test(url, dictionary_login):
         print(f"An error occurred: {e}")
         return
 
-resposta = login_test("http://localhost/login.php", {"username": "admin", "password": "password", "user_token" : })
+def token_extractor(url, token_name):
+    soup = html_extractor(url)
+    token = soup.find("input", {"name": token_name})['value']
+    return token
+
+user_token = token_extractor("http://localhost/login.php", "user_token")
+
+resposta = login_test("http://localhost/login.php", {"username": "admin", "password": "password", "user_token" : user_token})
 
 print(resposta.text)
--- src/recon/spider.py
+++ src/recon/spider.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 def html_extractor(url):
     try:
         html = get(url)
-        return html.text
+        return BeautifulSoup(html.text, "html.parser")
     except exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return
-
-def 
