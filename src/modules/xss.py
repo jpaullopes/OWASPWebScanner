34ef671342507bb9fbf68dfdf440ab832b8252d3
jpaullopes <jpaullopes27@gmail.com>
2025-08-20 18:45:53 -0300

    selenium was added in the project

--- src/modules/xss.py
+++ src/modules/xss.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 # Função responsável por encontrar as tags passadas como parâmetro
 def find_tags(html_content, tags):
@@ -8,3 +9,10 @@ def find_tags(html_content, tags):
         found_tags[tag] = [str(element) for element in soup.find_all(tag)]
     return found_tags
 
+html = requests.get("http://localhost:3000/#/login/").text
+print(html)
+
+# Exemplo de uso
+tags_to_find = ['input', 'form', 'textarea', 'select']
+found_tags = find_tags(html, tags_to_find)
+print(found_tags)
