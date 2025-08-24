0ef4083382fef7c6a29d8dc0c538d8d2a6013548
jpaullopes <jpaullopes27@gmail.com>
2025-08-24 17:20:31 -0300

    use exemple has been tested

--- src/modules/sql_injection/sql_injection.py
+++ src/modules/sql_injection/sql_injection.py
@@ -57,5 +57,11 @@ def sql_injection_test(payloads, login_url):
             
     return bypassed_payloads
 
-
+# Exemplo de uso
+url_login = "http://localhost:3000/#/login"
+bypassed = sql_injection_test(bypass_sql_injection_list, url_login)
+if bypassed:
+    print("\n--- Possíveis Injeções SQL Encontradas ---")
+    for field, payload, resp in bypassed:
+        print(f"Campo: {field} <|> Payload: {payload} <|> Email: {resp.get("authentication", {}).get("umail", "Não encontrado")}")
 
