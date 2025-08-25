c7ecb4e6c3d2db5c4a29946958e08b2699524d8a
jpaullopes <jpaullopes27@gmail.com>
2025-08-25 18:52:25 -0300

    the fucntion login_acess was modified

--- src/modules/broken_acess_control/login_acess.py
+++ src/modules/broken_acess_control/login_acess.py
@@ -9,25 +9,17 @@ EMAIL_LOGIN = os.getenv("EMAIL_LOGIN")
 PASSWORD_LOGIN = os.getenv("PASSWORD_LOGIN")
 
 
-def login_acess(url_login):
+def login_acess(page):
     """Realiza o login na página alvo com as credenciais que estão no arquivo environment."""
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch() 
-        page = browser.new_page()
-
-        # Navega para a página
-        page.goto(url_login)
-        
-        # Chama a função para fechar modais e popups
-        close_modals_and_popups(page)
-        
-        # Simula o preenchimento e envio do formulário
-        try:
-            page.locator("input[name='email']").fill(EMAIL_LOGIN)
-            page.locator("input[name='password']").fill(PASSWORD_LOGIN)
-            page.locator("input[name='password']").press('Enter')
-        except Exception as e:
-            print(f"Não foi possível preencher o formulário automaticamente: {e}")
-        
-        browser.close()
+    # Chama a função para fechar modais e popups
+    close_modals_and_popups(page)
+    
+    # Simula o preenchimento e envio do formulário
+    try:
+        page.locator("input[name='email']").fill(EMAIL_LOGIN)
+        page.locator("input[name='password']").fill(PASSWORD_LOGIN)
+        page.locator("input[name='password']").press('Enter')
+    except Exception as e:
+        print(f"Não foi possível preencher o formulário automaticamente: {e}")
+    
