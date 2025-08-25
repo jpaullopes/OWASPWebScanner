cadd84d04d4ed9ed20a085246c46d925ac3b9889
jpaullopes <jpaullopes27@gmail.com>
2025-08-25 14:38:25 -0300

    new function and module has been created

---
+++ src/modules/broken_acess_control/login_acess.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright
+from src.recon.web_crawler import close_modals_and_popups 
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+EMAIL_LOGIN = os.getenv("EMAIL_LOGIN")
+PASSWORD_LOGIN = os.getenv("PASSWORD_LOGIN")
+
+
+def login_acess(url_login):
+    """Realiza o login na página alvo com as credenciais que estão no arquivo environment."""
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch() 
+        page = browser.new_page()
+
+        # Navega para a página
+        page.goto(url_login)
+        
+        # Chama a função para fechar modais e popups
+        close_modals_and_popups(page)
+        
+        # Simula o preenchimento e envio do formulário
+        try:
+            page.locator("input[name='email']").fill(EMAIL_LOGIN)
+            page.locator("input[name='password']").fill(PASSWORD_LOGIN)
+            page.locator("input[name='password']").press('Enter')
+        except Exception as e:
+            print(f"Não foi possível preencher o formulário automaticamente: {e}")
+        
+        browser.close()
