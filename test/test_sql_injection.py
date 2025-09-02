4f6cde6c10d33dec1bdec1d38afb3686cddd4458
jpaullopes <jpaullopes27@gmail.com>
2025-09-01 21:01:05 -0300

    test(sql_injection): Update tests to reflect module refactoring

--- test/test_sql_injection.py
+++ test/test_sql_injection.py
@@ -1,16 +1,11 @@
-import sys
-import os
-import requests
-from playwright.sync_api import sync_playwright
-from src.modules.sql_injection.sql_injection import sql_injection_test, bypass_sql_injection_list
-
-# Adiciona o diretório raiz ao sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from src.modules.sql_injection import bypass_sql_injection_list, sql_injection_test
 
 url_login = "http://localhost:3000/#/login"
 bypassed = sql_injection_test(bypass_sql_injection_list, url_login)
 if bypassed:
     print("\n--- Possíveis Injeções SQL Encontradas ---")
     for field, payload, resp in bypassed:
-        print(f"Campo: {field} <|> Payload: {payload} <|> Email: {resp.get("authentication", {}).get("umail", "Não encontrado")}")
+        print(
+            f"Campo: {field} <|> Payload: {payload} <|> Email: "
+            f"{resp.get('authentication', {}).get('umail', 'Não encontrado')}"
+        )
