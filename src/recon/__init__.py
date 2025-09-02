3ce701f49903f640970966ed8ddc5a20c3b97732
jpaullopes <jpaullopes27@gmail.com>
2025-09-01 21:00:35 -0300

    feat: Expose recon module functions

---
+++ src/recon/__init__.py
@@ -0,0 +1,15 @@
+from .web_crawler import (
+    activate_search_bar,
+    close_modals_and_popups,
+    find_tags,
+    get_rendered_page,
+    page_reload,
+)
+
+__all__ = [
+    "page_reload",
+    "get_rendered_page",
+    "activate_search_bar",
+    "close_modals_and_popups",
+    "find_tags"
+]
