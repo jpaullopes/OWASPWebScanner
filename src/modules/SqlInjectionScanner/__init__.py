78a62c71b0f0c657a589db8b490b48b0164cf3d5
jpaullopes <jpaullopes27@gmail.com>
2025-09-24 20:14:18 -0300

    refactor(sqli): Expose SQLInjectionScanner in package

--- src/modules/SqlInjectionScanner/__init__.py
+++ src/modules/SqlInjectionScanner/__init__.py
@@ -1,10 +1,5 @@
-from .network_analisis import espionar_requisicao, find_login_api_url
-from .sql_injection import bypass_sql_injection_list, login_test, sql_injection_test
+from .sql_injection import SQLInjectionScanner
 
 __all__ = [
-    "espionar_requisicao",
-    "find_login_api_url",
-    "bypass_sql_injection_list",
-    "sql_injection_test",
-    "login_test"
+    "SQLInjectionScanner"
 ]
