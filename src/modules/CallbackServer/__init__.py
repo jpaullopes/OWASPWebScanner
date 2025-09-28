1546251e7c79a67b0444c122564e719a9726f9ce
jpaullopes <jpaullopes27@gmail.com>
2025-09-28 12:07:09 -0300

    refactor: map legacy callback package to new server

--- src/modules/CallbackServer/__init__.py
+++ src/modules/CallbackServer/__init__.py
@@ -1,13 +1,25 @@
+"""Compat package para o antigo módulo de callback."""
+
+from __future__ import annotations
+
 from .xss_http_server import (
-    XSSRequestHandler,
     iniciar_servidor_ouvinte,
+    limpar_tracking,
+    obter_payloads_executados,
+    obter_payloads_injetados,
+    obter_payloads_recebidos,
     obter_relatorio_detalhado,
+    obter_status_tracking,
     registrar_payload_injetado,
 )
 
 __all__ = [
     "iniciar_servidor_ouvinte",
-    "registrar_payload_injetado",
+    "limpar_tracking",
+    "obter_payloads_executados",
+    "obter_payloads_injetados",
+    "obter_payloads_recebidos",
     "obter_relatorio_detalhado",
-    "XSSRequestHandler",
+    "obter_status_tracking",
+    "registrar_payload_injetado",
 ]
