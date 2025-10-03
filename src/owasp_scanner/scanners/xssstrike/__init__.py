02466975fa11bd91c3efaaa9e618d40500e99463
jpaullopes <jpaullopes27@gmail.com>
2025-10-03 11:05:06 -0300

    docs: mark xssstrike package as deprecated

--- src/owasp_scanner/scanners/xssstrike/__init__.py
+++ src/owasp_scanner/scanners/xssstrike/__init__.py
@@ -1,5 +1,1 @@
-"""Integration layer for running XSSStrike against discovered targets."""
-
-from .runner import XSSStrikeRunResult, run_xssstrike_scanner
-
-__all__ = ["XSSStrikeRunResult", "run_xssstrike_scanner"]
+"""Deprecated XSSStrike integration module (intentionally left blank)."""
