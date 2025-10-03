e12f0cfb622c501424f9d6988c640948c32ca549
jpaullopes <jpaullopes27@gmail.com>
2025-10-03 10:57:46 -0300

    chore: remove legacy xss scanner init

--- src/modules/XssScanner/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Compat package para o antigo módulo de XSS."""
-
-from __future__ import annotations
-
-from .scanner import XSSScanner
-from owasp_scanner.scanners.xss.runner import run_xss_scanner as run_xss_scan
-
-__all__ = ["XSSScanner", "run_xss_scan"]
