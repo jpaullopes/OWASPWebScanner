8e81a5fcb2692eb906ab4dba2143da8be7b67785
jpaullopes <jpaullopes27@gmail.com>
2025-10-03 11:07:04 -0300

    style: wrap config imports in helper module

--- tests/helpers/owasp_imports.py
+++ tests/helpers/owasp_imports.py
@@ -1,9 +1,12 @@
 """Centralized imports for the OWASP scanner package used in tests."""
 
 from owasp_scanner.access import analyzer  # type: ignore[import]
-from owasp_scanner.core.config import load_configuration, ScannerConfig  # type: ignore[import]
-from owasp_scanner.core.report import ReconReport  # type: ignore[import]
 from owasp_scanner.core import dependencies  # type: ignore[import]
+from owasp_scanner.core.config import (  # type: ignore[import]
+    ScannerConfig,
+    load_configuration,
+)
+from owasp_scanner.core.report import ReconReport  # type: ignore[import]
 from owasp_scanner.recon.utils import build_cookie_header  # type: ignore[import]
 from owasp_scanner.scanners.sql import runner as sql_runner  # type: ignore[import]
 
