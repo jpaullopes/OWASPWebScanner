834c8cbb7b18f6a3c825dfc8036748de9ddd03dd
jpaullopes <jpaullopes27@gmail.com>
2025-10-03 10:56:56 -0300

    chore: remove legacy sql scanner init

--- src/modules/SqlInjectionScanner/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Compat package para o antigo módulo de SQL Injection."""
-
-from __future__ import annotations
-
-from .sql_injection import format_cookies, run_sqli_scan
-
-__all__ = ["format_cookies", "run_sqli_scan"]
