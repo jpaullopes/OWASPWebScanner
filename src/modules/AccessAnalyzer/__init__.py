05bf2a5de121e2fc49704f220f07a48326d09a7b
jpaullopes <jpaullopes27@gmail.com>
2025-10-03 10:47:53 -0300

    chore: remove legacy AccessAnalyzer package init

--- src/modules/AccessAnalyzer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Compat package para código que ainda importa ``src.modules.AccessAnalyzer``."""
-
-from __future__ import annotations
-
-from .url_scan import url_scanner
-
-__all__ = ["url_scanner"]
